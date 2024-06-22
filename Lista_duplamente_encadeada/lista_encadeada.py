07a8e0ecaddd4f19d59d7321e29b9c248abc948b
William Tavares <williamtavaresdemoura@gmail.com>
2024-06-22 14:39:01 -0300

    Criando o metodo de pegar os elementos da nossa lista

--- Lista_duplamente_encadeada/lista_encadeada.py
+++ Lista_duplamente_encadeada/lista_encadeada.py
@@ -5,41 +5,51 @@
 # onde ele sabe o proximo elemento da lista
 
 class Lista:
-  class No:
-    def __init__(self, valor, proximo=None):
-      self.valor = valor
-      self.proximo = None
+    class No:
+        def __init__(self, valor, proximo=None):
+            self.valor = valor
+            self.proximo = None
   
   
-  def __init__(self):
-    self.__cabeca = None
-    self.__quantidade = 0
+    def __init__(self):
+        self.__cabeca = None
+        self.__quantidade = 0
     
-  def __len__(self):
-    return self.__quantidade
+    def __getitem__(self, posicao):
+        if posicao < 0 or posicao >= self.__quantidade:
+            raise IndexError("Posição invélida")
+    
+        atual = self.__cabeca
+        for i in range(posicao):
+            atual = atual.proximo  
+
+        return atual.valor
+    
+    def __len__(self):
+        return self.__quantidade
   
-  def inserir(self, posicao, valor):
-    novo = self.No(valor)
-    self.__quantidade += 1
-    # Quando a lista é vazia
-    if self.__cabeca is None:
-      self.__cabeca = novo
-      return
+    def inserir(self, posicao, valor):
+        novo = self.No(valor)
+        self.__quantidade += 1
+        # Quando a lista é vazia
+        if self.__cabeca is None:
+            self.__cabeca = novo
+            return
     
     # Inserir na cabeca (primeira posicao)
-    if posicao <= 0:
-      novo.proximo = self.__cabeca
-      self.__cabeca = novo
-      return
+        if posicao <= 0:
+            novo.proximo = self.__cabeca
+            self.__cabeca = novo
+            return
     
-    i = 0
-    atual = self.__cabeca
-    while atual.proximo is not None and i < posicao -1:
-      atual = atual.proximo
-      i+=1
+        i = 0
+        atual = self.__cabeca
+        while atual.proximo is not None and i < posicao -1:
+            atual = atual.proximo
+            i+=1
     
-    novo.proximo = atual.proximo
-    atual.proximo = novo
+        novo.proximo = atual.proximo
+        atual.proximo = novo
 
 
 lista = Lista()
@@ -48,3 +58,6 @@ lista.inserir(1, 20)
 lista.inserir(1, 15)
 lista.inserir(1, 10)
 print(len(lista))
+print(f'O elemento na posicao 0 é {lista[3]}')
+for i in lista:
+    print(i)
